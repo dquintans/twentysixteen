5ede24586313ee6553effbaba8871cecf1e4bbaa
dquintans <dquintans3@gmail.com>
2016-07-19 11:16:20 +0800

    Update 404.php
    Edit from remote

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The template for displaying 404 pages (not found)
- * Testing Dines remote publish 2
+ * Testing Dines remote publish 2 / Edit from remote repository
  * @package WordPress
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
